0ce88e5d06fb7510936be8f1cf5058444aa1075a
Kazi Shahryar Rahman <159161539+shahryar2312@users.noreply.github.com>
2026-05-06 18:27:02 -0700

    Delete gamerhub-webdev-year3 directory
    just came in side of the file

--- gamerhub-webdev-year3/backend/src/routes/search.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { globalSearch } = require('../controllers/search.controller');
-
-// @route   GET /api/search
-router.get('/', globalSearch);
-
-module.exports = router;
