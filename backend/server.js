8c583d66c491010121317fd60aecdb3807a5fb36
kazishahryar.rahman <kazishahryar.rahman@student.griffith.ie>
2026-05-07 20:29:53 -0700

    fixed for render

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax', // Use 'lax' for better cross-site experience with cookies
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     },
   })
